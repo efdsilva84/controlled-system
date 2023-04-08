9e9ca1e0179613ca52fa97daba22e59bcdf84a57
edcarlos <efdsilva84@gmail.com>
2023-04-08 19:54:37 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CadUserComponent } from './pages/cad-user/cad-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'cad-user', component: CadUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
